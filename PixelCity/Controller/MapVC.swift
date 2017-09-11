feebe6fa3630bf8c553328ab65fb010644ff9bf0
Roy Dimayuga <mr_nicelittleboy@yahoo.com>
2017-09-11 16:43:14 -0700

    Implement UI

--- PixelCity/Controller/MapVC.swift
+++ PixelCity/Controller/MapVC.swift
@@ -8,29 +8,26 @@
 
 import UIKit
 import Alamofire
+import MapKit
 
 class MapVC: UIViewController {
-
+    
+    // MARK: Outlets
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        mapView.delegate = self
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func centerMapBtnPressed(_ sender: Any) {
+        
     }
     
 
-    /*
-    // MARK: - Navigation
+}
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+extension MapVC: MKMapViewDelegate {
+    
 }
